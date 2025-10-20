f681c60e7e415f6b0e6b797b144d28df60f1ec6d
amanrent <amanrentc@gmail.com>
2025-10-20 21:20:16 +0700

    Commit

--- index.php
+++ index.php
@@ -135,7 +135,7 @@
             <a href="https://wa.me/6285975287684" class="btn-primary" target="_blank" rel="noopener noreferrer">Konsultasi Pembelian</a>
           </div>
         </div>
-        <img src="images/hino.png" alt="Truk Hino Hijau" loading="lazy" class="promo-main-image"/>
+        <img src="img/hino.png" alt="Truk Hino Hijau" loading="lazy" class="promo-main-image"/>
       </section>
       
       <!-- Fitur -->
