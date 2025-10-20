630139ed1f7551fe75b20245878606d48d9ca592
amanrent <amanrentc@gmail.com>
2025-10-20 20:21:37 +0700

    Commit

---
+++ footer.php
@@ -0,0 +1,46 @@
+<?php
+// footer.php
+?>
+    <!-- Footer -->
+    <footer class="site-footer">
+      <div class="footer-container">
+        <div class="footer-section">
+          <img src="img/logo3.png" alt="Logo" class="footer-logo" />
+          <p>Dealer Hino Official – Mitra Terpercaya untuk Kebutuhan Kendaraan Niaga Anda.</p>
+        </div>
+        <div class="footer-section">
+          <h4>HUBUNGI KAMI</h4>
+          <p>📞 0812-1905-5571</p>
+          <p>📧 denishinoindonesia@gmail.com</p>
+          <p>📍 Golf Lake Ruko Venice, Jl. Lkr. Luar Barat No.78 Blok B, RT.9/RW.14, Cengkareng Tim., Kecamatan Cengkareng, Jakarta</p>
+          <div class="footer-social">
+            <h4>SOSIAL MEDIA</h4>
+          <div class="social-icons">
+            <a href="https://www.instagram.com/saleshinojabodetabek" target="_blank" aria-label="Instagram Sales Hino Jabodetabek">
+              <i data-feather="instagram"></i>
+            </a>
+            <a href="https://wa.me/+6281219055571?text=Halo%20Saya%20Dapat%20Nomor%20Anda%20Dari%20Google" target="_blank" aria-label="WhatsApp Sales Hino Jabodetabek">
+              <i data-feather="phone"></i>
+            </a>
+            <a href="https://www.facebook.com/profile.php?id=61573843992250" target="_blank" aria-label="Facebook Sales Hino Jabodetabek">
+              <i data-feather="facebook"></i>
+            </a>
+          </div>
+          </div>
+        </div>
+        <div class="footer-section">
+          <div class="google-map-container">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63471.95288843176!2d106.65860738294855!3d-6.131096504333846!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6a1d775401fa6d%3A0xc7e25a8d81b821ec!2sDealer%20Hino%20Jabodetabek%20Resmi!5e0!3m2!1sen!2sus!4v1760817261750!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          </div>
+        </div>
+      </div>
+      <div class="footer-bottom">
+        <p>&copy; 2025 Official Hino. All Rights Reserved.</p>
+      </div>
+    </footer>
+
+    <!-- CSS Footer -->
+    <link rel="stylesheet" href="css/footer.css" />
+
+  </body>
+</html>
