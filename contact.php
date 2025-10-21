fdb9c45b3f33bc81dc358f8fcde3b59cb68f87f2
amanrent <amanrentc@gmail.com>
2025-10-21 15:14:20 +0700

    Commit

--- contact.php
+++ contact.php
@@ -1,10 +1,5 @@
 <?php
-// Aktifkan error reporting
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-require_once 'config.php'; // pastikan file ini memuat koneksi PDO
+require_once 'admin/config.php'; // pastikan file ini memuat koneksi PDO
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     // Ambil dan bersihkan input
